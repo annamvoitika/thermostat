b918ebb13b4c02a345830795e29a4364f75bb63a
Anna <anna.voitik@gmail.com>
2020-06-02 17:03:00 +0100

    seven user stories done

---
+++ jasmine-standalone-3.5.0/src/thermostat.js
@@ -0,0 +1,38 @@
+class Thermostat {
+  constructor(){
+    this.degrees = 20;
+    this.mode = "on";
+    this.max_power_saving_mode_on = 25;
+    this.max_power_saving_mode_off = 32;
+  }
+
+  degree(number){
+    this.degrees = number
+    return this.degrees;
+  }
+
+  up(number){
+    this.degrees = this.degrees + number
+      if (this.mode == "on" && this.degrees > this.max_power_saving_mode_on){
+      throw "Power saving mode is on, max temp reached";
+    }
+      else if (this.mode == "off" && this.degrees > this.max_power_saving_mode_off){
+        throw "Power saving mode is off, max temp reached";
+      }
+      else {
+    return this.degrees;}
+  }
+
+  down(number){
+    if (this.degrees - number < 10){
+      throw "Temperature too low!";
+    }
+    else {
+    this.degrees = this.degrees - number
+    return this.degrees;}
+  }
+
+  mode_status(){
+    return this.mode;
+  }
+};
